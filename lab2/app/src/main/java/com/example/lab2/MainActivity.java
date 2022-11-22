489f47cfa7a95a7f6d569ef98ee94c0f193e49f0
Egor <edbaturin@edu.hse.ru>
2022-11-22 18:01:18 +0300

    lab2 fix

--- lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onConfigurationChanged(Configuration newConfig) {
+        Configuration config = new Configuration();
+        if (newConfig.keyboardHidden == Configuration.KEYBOARDHIDDEN_NO) {
+            config.keyboardHidden = Configuration.KEYBOARDHIDDEN_NO;
+            Toast.makeText(getApplicationContext(), "The keyboard has changed state.", Toast.LENGTH_LONG).show();
+        }
+        super.onConfigurationChanged(config);
+    }
+
 }
